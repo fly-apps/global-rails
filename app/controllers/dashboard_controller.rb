b22116b43bceeacf354917fcc8181c4cb660ca34
Kurt <mrkurt@gmail.com>
2021-09-16 19:00:21 -0500

    endpoint for trying large post bodies

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+  skip_forgery_protection
   def region
     current = ENV['FLY_REGION']
     r = params[:code].to_s
@@ -13,4 +14,19 @@ class DashboardController < ApplicationController
   #  render plain: "We got weird data from your browser, this is an error", status: 200
   end
 
+  def echo
+    primary = ENV['PRIMARY_REGION'] || "local"
+    current = ENV['FLY_REGION']
+
+    if primary != current || request.headers['Fly-Region'] == 'remote'
+      response.headers["fly-replay"] = "region=#{primary}"
+      Rails.logger.info "retry in region #{primary}"
+      render plain: "retry in region #{primary}", status: 409
+    else
+      render json: {
+        wat: "yes",
+        body_length: request.raw_post&.length.to_i
+      }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/regions/:code" => "dashboard#region"
+
+  post "/echo" => "dashboard#echo"
   
   resources :timing_results, only: [:index, :create]
   root to: "dashboard#index"
